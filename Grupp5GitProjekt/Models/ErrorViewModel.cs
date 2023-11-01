a086b16ccd6e2916e4a53dcd19b6508c5615ed75
Samuel Lidström <samuel.lidstrom@gmail.com>
2023-11-01 15:35:53 +0100

    Add project files.

---
+++ Grupp5GitProjekt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Grupp5GitProjekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
