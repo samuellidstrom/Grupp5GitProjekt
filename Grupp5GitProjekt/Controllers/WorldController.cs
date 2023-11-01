ea2760f44545b0aba245e220f319950702bb79d9
Samuel Lidström <samuel.lidstrom@gmail.com>
2023-11-01 15:50:29 +0100

    Added world controller

---
+++ Grupp5GitProjekt/Controllers/WorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Grupp5GitProjekt.Controllers
+{
+    public class WorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
